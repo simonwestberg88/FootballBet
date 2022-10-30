da97d67a8a319d8cf2745ca5918daeb74f39e52b
simon westberg <simon.westberg88@gmail.com>
2022-10-30 20:33:03 +0100

    added test proj

---
+++ TestProject/Program.cs
@@ -0,0 +1,31 @@
+using FootballBet.Infrastructure;
+using FootballBet.Infrastructure.Interfaces;
+using FootballBet.Infrastructure.Settings;
+using FootballBet.Repository;
+using FootballBet.Repository.Entities;
+using FootballBet.Repository.Repositories;
+using FootballBet.Server.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using TestProject.TestApi;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedAccount = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddIdentityServer()
+    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+builder.Services.Configure<FootballApiSettings>(builder.Configuration.GetSection("FootballApi"));
+builder.Services.AddSingleton<IFootballApiClient, FootballApiClient>();
+builder.Services.AddHttpClient<FootballApiClient>();
+builder.Services.AddScoped<IFootballRepository, FootballRepository>();
+builder.Services.AddScoped<IFootballAPIService, FootballAPIService>();
+var app = builder.Build();
+app.AddTestApi();
+
+app.Run();
---
+++ TestProject/TestApi/ApiWebApplicationExtension.cs
@@ -0,0 +1,26 @@
+using FootballBet.Infrastructure.Interfaces;
+
+namespace TestProject.TestApi;
+
+public static class ApiWebApplicationExtension
+{
+    public static void AddTestApi(this WebApplication app)
+    {
+        app.MapGet("/test/fixtures", async (IFootballApiClient client) =>
+        {
+            var fixtures = await client.GetFixtures(1, "2018");
+            return fixtures;
+        });
+        
+        app.MapGet("/test/leagues", async (IFootballApiClient client) =>
+        {
+            var league = await client.GetSpecificLeague("1");
+            return league;
+        });
+
+        app.MapPost("test/seed", async (IFootballAPIService footballService) =>
+        {
+            await footballService.SeedDatabase();
+        });
+    }
+}
