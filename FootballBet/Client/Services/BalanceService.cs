9323acb44c5268c027aa9d8c67b33ded2e047bb1
simon westberg <simon.westberg88@gmail.com>
2022-11-03 08:21:46 +0100

    balance service in client

--- FootballBet/Client/Services/BalanceService.cs
+++ FootballBet/Client/Services/BalanceService.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 namespace FootballBet.Client.Services;
 public interface IBalanceService
 {
@@ -5,7 +7,32 @@ public interface IBalanceService
     Task<decimal> DepositAsync(decimal amount);
     Task<decimal> WithdrawAsync(decimal amount);
 }
-public class BalanceService
+public class BalanceService: IBalanceService
 {
-    
+    private readonly HttpClient _httpClient;
+    public BalanceService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<decimal> GetBalanceAsync()
+    {
+        var response = await _httpClient.GetAsync("api/balance");
+        response.EnsureSuccessStatusCode();
+        var balance = await response.Content.ReadFromJsonAsync<decimal>();
+        return balance;
+    }
+    public async Task<decimal> DepositAsync(decimal amount)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/balance/deposit", amount);
+        response.EnsureSuccessStatusCode();
+        var balance = await response.Content.ReadFromJsonAsync<decimal>();
+        return balance;
+    }
+    public async Task<decimal> WithdrawAsync(decimal amount)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/balance/withdraw", amount);
+        response.EnsureSuccessStatusCode();
+        var balance = await response.Content.ReadFromJsonAsync<decimal>();
+        return balance;
+    }
 }
