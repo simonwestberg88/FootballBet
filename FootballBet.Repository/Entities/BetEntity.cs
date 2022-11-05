b5f9402e44deb4662057e3b4d8b10c07338355b3
simon westberg <simon.westberg88@gmail.com>
2022-11-05 19:34:53 +0100

    updating betting entity

--- FootballBet.Repository/Entities/BetEntity.cs
+++ FootballBet.Repository/Entities/BetEntity.cs
@@ -6,17 +6,9 @@ public class BetEntity
 {
     public int Id { get; set; }
     public int? MatchId { get; set; }
-    [ForeignKey("MatchId")]
-    public virtual MatchEntity Match { get; set; }
     public string? UserId { get; set; }
-    [ForeignKey("UserId")]
-    public virtual ApplicationUser User { get; set; }
     public Guid? BettingGroupId { get; set; }
-    [ForeignKey("BettingGroupId")]
-    public virtual BettingGroupEntity? BettingGroup { get; set; }
     public int? OddsId { get; set; }
-    [ForeignKey("OddsId")]
-    public virtual OddsEntity OddsEntity { get; set; }
     public decimal WagerAmount { get; set; }
     public decimal PaybackAmount { get; set; }
     public bool? IsWinningBet { get; set; }
