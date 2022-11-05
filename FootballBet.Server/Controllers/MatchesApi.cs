86db1a3285c4ce107d117fb1f904fb62016c5542
simon westberg <simon.westberg88@gmail.com>
2022-11-05 19:34:41 +0100

    add endpoint for getting odds

--- FootballBet.Server/Controllers/MatchesApi.cs
+++ FootballBet.Server/Controllers/MatchesApi.cs
@@ -7,7 +7,9 @@ public static class MatchesApi
     public static void AddMatchesApi(this WebApplication app)
     {
         app.MapGet("api/matches/{leagueId:int}", (int leagueId, IFootballAPIService footballApiService) =>
-             footballApiService.GetMatches(leagueId)
-        ).AllowAnonymous();
+            footballApiService.GetMatches(leagueId)).AllowAnonymous();
+
+        app.MapGet("api/matches/{matchId:int}/odds", async (int matchId, IFootballApiClient client)
+            => await client.GetLatestOddsForMatch(matchId)).AllowAnonymous();
     }
 }
