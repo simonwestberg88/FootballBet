851f656a13d690fcef4f422981c86c1dd9e5f79e
simon westberg <simon.westberg88@gmail.com>
2022-11-06 16:18:54 +0100

    add sum memory caching

--- FootballBet.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ FootballBet.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -23,6 +23,7 @@ public static class ServiceCollectionExtensions
             .AddTransient<IUserRepository, UserRepository>()
             .AddTransient<ITransactionService, TransactionService>()
             .AddTransient<IBetService, BetService>()
+            .AddMemoryCache()
             .AddRepositories();
 
     private static IServiceCollection AddRepositories(this IServiceCollection services)
--- FootballBet.Server/Controllers/OddsApi.cs
+++ FootballBet.Server/Controllers/OddsApi.cs
@@ -1,4 +1,6 @@
 using FootballBet.Infrastructure.Interfaces;
+using FootballBet.Shared.Models.Odds;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace FootballBet.Server.Controllers;
 
@@ -6,7 +8,16 @@ public static class OddsApi
 {
     public static void AddOddsApi(this WebApplication app)
     {
-        app.MapGet("api/matches/odds", async (int matchId, IFootballApiClient client) => 
-            await client.GetLatestOddsForMatch(matchId)).AllowAnonymous();
+        app.MapGet("api/matches/odds", async (int matchId, IFootballApiClient client, IMemoryCache cache) =>
+        {
+            if (cache.TryGetValue(matchId, out IEnumerable<OddsDto> cachedOdds))
+            {
+                return cachedOdds;
+            }
+
+            var odds = (await client.GetLatestOddsForMatch(matchId)).ToList();
+            cache.Set(matchId, odds, TimeSpan.FromHours(1));
+            return odds;
+        }).AllowAnonymous();
     }
 }
