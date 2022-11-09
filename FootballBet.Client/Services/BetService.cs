432754d37ce26bca3aa09207ced0ffab58b02d1d
simon westberg <simon.westberg88@gmail.com>
2022-11-09 14:27:04 +0100

    placing bets

--- FootballBet.Client/Services/BetService.cs
+++ FootballBet.Client/Services/BetService.cs
@@ -5,7 +5,7 @@ namespace FootballBet.Client.Services;
 public interface IBetService
 {
     Task<IEnumerable<BetDto>> GetBetsAsync(string groupId);
-    Task<BetDto> PlaceBetAsync(int oddsId, decimal amount);
+    Task<BetDto> PlaceBetAsync(int oddsId, decimal amount, string groupId);
 }
 public class BetService : IBetService
 {
@@ -20,14 +20,14 @@ public class BetService : IBetService
         throw new NotImplementedException();
     }
 
-    public async Task<BetDto> PlaceBetAsync(int oddsId, decimal amount)
+    public async Task<BetDto> PlaceBetAsync(int oddsId, decimal amount, string groupId)
     {
         var bet = new BetDto
         {
             OddsId = oddsId,
             Amount = amount
         };
-        var response = await _httpClient.PostAsJsonAsync($"api/bets/place?groupId={1}", bet);
+        var response = await _httpClient.PostAsJsonAsync($"api/bets/place?groupId={groupId}", bet);
         return await response.Content.ReadFromJsonAsync<BetDto>();
     }
 }
