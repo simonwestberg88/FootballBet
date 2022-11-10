1db5368a4f7f7cf5159b46277134edd49b9fb330
simon westberg <simon.westberg88@gmail.com>
2022-11-10 12:12:53 +0100

    db update

--- FootballBet.Repository/Entities/UserBalanceEntity.cs
+++ FootballBet.Repository/Entities/UserBalanceEntity.cs
@@ -2,6 +2,7 @@ namespace FootballBet.Repository.Entities;
 
 public class UserBalanceEntity
 {
+    public int Id { get; set; }
     public string UserId { get; set; }
     public decimal Balance { get; set; }
     public string GroupId { get; set; }
