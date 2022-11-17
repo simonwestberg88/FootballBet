d6bff80fc33a230355603ef98679aa016a2dd535
Gustav Westberg <gustavwes@gmail.com>
2022-11-17 17:07:49 +0100

    new domain footballbet.online

--- FootballBet.Infrastructure/DependencyInjection/WebApplicationBuilderExtensions.cs
+++ FootballBet.Infrastructure/DependencyInjection/WebApplicationBuilderExtensions.cs
@@ -31,7 +31,7 @@ public static class WebApplicationBuilderExtensions
 
         builder.Services.AddIdentityServer(options =>
         {
-            options.IssuerUri = "https://footballbet-app.azurewebsites.net/";
+            options.IssuerUri = "https://footballbet.online/";
         })
             .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
     }
