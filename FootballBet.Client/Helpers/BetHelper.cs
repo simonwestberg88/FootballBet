b443a0d3fc3f2393f1b5e2473ba274884ec500ed
kratli <gustavwes@gmail.com>
2022-11-23 18:57:29 +0100

    Further fix for colorcoding bet results. Moved methods to helpers

---
+++ FootballBet.Client/Helpers/BetHelper.cs
@@ -0,0 +1,18 @@
+﻿using FootballBet.Shared.Models.Bets;
+using FootballBet.Shared.Models.Match;
+
+namespace FootballBet.Client.Helpers
+{
+    public static class BetHelper
+    {
+        public static string GetBackGroundColorDependingOnStatus(MatchDto match, BetResponse userBet)
+            => match.MatchStatus == "Match Finished"
+                ? MatchHelper.GetTypeOfWin(match, userBet) switch
+                {
+                    "exact" => "#90F166",
+                    "base" => "#EBF166",
+                    _ => "#F58702"
+                }
+                : "#FFFFFF";
+    }
+}
---
+++ FootballBet.Client/Helpers/MatchHelper.cs
@@ -0,0 +1,21 @@
+﻿using FootballBet.Shared.Models.Bets;
+using FootballBet.Shared.Models.Match;
+
+namespace FootballBet.Client.Helpers
+{
+    public static class MatchHelper
+    {
+        public static string GetTypeOfWin(MatchDto match, BetResponse userBet)
+        {
+            if (match.AwayFulltimeGoals == userBet.AwayGoals && match.HomeFulltimeGoals == userBet.HomeGoals)
+                return "exact";
+            //base win
+            if ((match.AwayFulltimeGoals < match.HomeFulltimeGoals && userBet.AwayGoals < userBet.HomeGoals)
+                || (match.AwayFulltimeGoals > match.HomeFulltimeGoals && userBet.AwayGoals > userBet.HomeGoals)
+                || (match.AwayFulltimeGoals == match.HomeFulltimeGoals && userBet.AwayGoals == userBet.HomeGoals))
+                return "base";
+            //lost
+            return "lost";
+        }
+    }
+}
