c5a7665f4833076aa0d53a7ae31956fd9f0c7fed
simon westberg <simon.westberg88@gmail.com>
2022-12-04 17:11:30 +0100

    db migration

---
+++ FootballBet.Repository/Migrations/20221204160801_AddWinEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballBet.Repository.Migrations
+{
+    public partial class AddWinEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WinEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MatchId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    WinDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsExactScoreWin = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WinEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WinEntities");
+        }
+    }
+}
